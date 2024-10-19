a63734dedd5c1d471c87d5bd31b0551e9beb7e3c
EnmaGalindo <145729033+EnmaGalindo@users.noreply.github.com>
2024-10-19 17:53:41 -0600

    Revert "Addition the MLFLow tracking for parameters, models and experiments - EG"

--- src/stages/evaluation.py
+++ src/stages/evaluation.py
@@ -8,13 +8,14 @@ import joblib
 def evaluation(path):
     with open(path) as con:
         config = yaml.safe_load(con)
-    models =  joblib.load(config['models']['path_models_class'])  
+    evaluator = Models(path)
+    models = joblib.load(config['models']['path_models'])
     file = config['processed']['path_xtest']
     Xtest = pd.read_csv(file, sep=';')
     file = config['datasets']['path_ytest']
     ytest = pd.read_csv(file, sep=';')
-    models.evaluate(Xtest, ytest)
-    
+    evaluator.evaluate(Xtest, ytest, models)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Models evaluation step.')
     parser.add_argument('--config', help='Path to the YAML configuration file.')
